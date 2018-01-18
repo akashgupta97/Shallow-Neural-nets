2b083e47b3bddd79786633a524f6c6a07017cd11
Akash <akash.darknight@gmail.com>
2018-01-18 10:03:00 +0530

    Main Script

--- Neuralnets.py
+++ Neuralnets.py
@@ -367,3 +367,141 @@ def compute_cost(A2, Y, parameters):
 A2, Y_assess, parameters = compute_cost_test_case()
 
 print("cost = " + str(compute_cost(A2, Y_assess, parameters)))
+
+# **Expected Output**:
+# <table style="width:20%">
+#   <tr>
+#     <td>**cost**</td>
+#     <td> 0.692919893776 </td>
+#   </tr>
+#
+# </table>
+
+# Using the cache computed during forward propagation, you can now implement backward propagation.
+#
+# **Question**: Implement the function `backward_propagation()`.
+#
+# **Instructions**:
+# Backpropagation is usually the hardest (most mathematical) part in deep learning. To help you, here again is the slide from the lecture on backpropagation. You'll want to use the six equations on the right of this slide, since you are building a vectorized implementation.
+#
+# <img src="images/grad_summary.png" style="width:600px;height:300px;">
+#
+# <!--
+# $\frac{\partial \mathcal{J} }{ \partial z_{2}^{(i)} } = \frac{1}{m} (a^{[2](i)} - y^{(i)})$
+#
+# $\frac{\partial \mathcal{J} }{ \partial W_2 } = \frac{\partial \mathcal{J} }{ \partial z_{2}^{(i)} } a^{[1] (i) T} $
+#
+# $\frac{\partial \mathcal{J} }{ \partial b_2 } = \sum_i{\frac{\partial \mathcal{J} }{ \partial z_{2}^{(i)}}}$
+#
+# $\frac{\partial \mathcal{J} }{ \partial z_{1}^{(i)} } =  W_2^T \frac{\partial \mathcal{J} }{ \partial z_{2}^{(i)} } * ( 1 - a^{[1] (i) 2}) $
+#
+# $\frac{\partial \mathcal{J} }{ \partial W_1 } = \frac{\partial \mathcal{J} }{ \partial z_{1}^{(i)} }  X^T $
+#
+# $\frac{\partial \mathcal{J} _i }{ \partial b_1 } = \sum_i{\frac{\partial \mathcal{J} }{ \partial z_{1}^{(i)}}}$
+#
+# - Note that $*$ denotes elementwise multiplication.
+# - The notation you will use is common in deep learning coding:
+#     - dW1 = $\frac{\partial \mathcal{J} }{ \partial W_1 }$
+#     - db1 = $\frac{\partial \mathcal{J} }{ \partial b_1 }$
+#     - dW2 = $\frac{\partial \mathcal{J} }{ \partial W_2 }$
+#     - db2 = $\frac{\partial \mathcal{J} }{ \partial b_2 }$
+#
+# !-->
+#
+# - Tips:
+#     - To compute dZ1 you'll need to compute $g^{[1]'}(Z^{[1]})$. Since $g^{[1]}(.)$ is the tanh activation function, if $a = g^{[1]}(z)$ then $g^{[1]'}(z) = 1-a^2$. So you can compute
+#     $g^{[1]'}(Z^{[1]})$ using `(1 - np.power(A1, 2))`.
+
+# In[74]:
+
+# GRADED FUNCTION: backward_propagation
+
+def backward_propagation(parameters, cache, X, Y):
+    """
+    Implement the backward propagation using the instructions above.
+
+    Arguments:
+    parameters -- python dictionary containing our parameters
+    cache -- a dictionary containing "Z1", "A1", "Z2" and "A2".
+    X -- input data of shape (2, number of examples)
+    Y -- "true" labels vector of shape (1, number of examples)
+
+    Returns:
+    grads -- python dictionary containing your gradients with respect to different parameters
+    """
+    m = X.shape[1]
+
+    # First, retrieve W1 and W2 from the dictionary "parameters".
+    ### START CODE HERE ### (≈ 2 lines of code)
+    W1 = parameters["W1"]
+    W2 = parameters["W2"]
+    ### END CODE HERE ###
+
+    # Retrieve also A1 and A2 from dictionary "cache".
+    ### START CODE HERE ### (≈ 2 lines of code)
+    A1 = cache["A1"]
+    A2 = cache["A2"]
+    ### END CODE HERE ###
+
+    # Backward propagation: calculate dW1, db1, dW2, db2.
+    ### START CODE HERE ### (≈ 6 lines of code, corresponding to 6 equations on slide above)
+    dZ2 = A2 - Y
+    dW2 = 1 / m * (np.dot(dZ2, A1.T))
+    db2 = 1 / m * (np.sum(dZ2, axis=1, keepdims=True))
+    dZ1 = np.dot(W2.T, dZ2) * (1 - np.power(A1, 2))
+    dW1 = 1 / m * (np.dot(dZ1, X.T))
+    db1 = 1 / m * (np.sum(dZ1, axis=1, keepdims=True))
+    ### END CODE HERE ###
+
+    grads = {"dW1": dW1,
+             "db1": db1,
+             "dW2": dW2,
+             "db2": db2}
+
+    return grads
+
+
+# In[75]:
+
+parameters, cache, X_assess, Y_assess = backward_propagation_test_case()
+
+grads = backward_propagation(parameters, cache, X_assess, Y_assess)
+print("dW1 = " + str(grads["dW1"]))
+print("db1 = " + str(grads["db1"]))
+print("dW2 = " + str(grads["dW2"]))
+print("db2 = " + str(grads["db2"]))
+
+
+# **Expected output**:
+#
+#
+#
+# <table style="width:80%">
+#   <tr>
+#     <td>**dW1**</td>
+#     <td> [[ 0.01018708 -0.00708701]
+#  [ 0.00873447 -0.0060768 ]
+#  [-0.00530847  0.00369379]
+#  [-0.02206365  0.01535126]] </td>
+#   </tr>
+#
+#   <tr>
+#     <td>**db1**</td>
+#     <td>  [[-0.00069728]
+#  [-0.00060606]
+#  [ 0.000364  ]
+#  [ 0.00151207]] </td>
+#   </tr>
+#
+#   <tr>
+#     <td>**dW2**</td>
+#     <td> [[ 0.00363613  0.03153604  0.01162914 -0.01318316]] </td>
+#   </tr>
+#
+#
+#   <tr>
+#     <td>**db2**</td>
+#     <td> [[ 0.06589489]] </td>
+#   </tr>
+#
+# </table>
